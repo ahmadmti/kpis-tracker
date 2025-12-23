0d5bd4b9860bc9cb0c447cf6f52a8d352aa37d2c
Ahmad Fraz <ahmadmti@yahoo.com>
2025-12-23 17:16:22 +0500

    API URL updated with RENDER

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # Constants
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://kpis-tracker-bg.onrender.com"
 
 st.set_page_config(
     page_title="System Dashboard",
